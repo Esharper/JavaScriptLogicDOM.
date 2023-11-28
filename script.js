58316a5f4de5fe25ba9657779b9d17074be74048
Elgin <elginsharper87@gmail.com>
2023-11-27 22:51:44 -0600

    user input fix

--- script.js
+++ script.js
@@ -64,6 +64,36 @@ if(newExample > 30) {
     console.log('the value is between 10 and 30');
 }
 
+console.log('hello');
+
+
+
+const userInputField= document.getElementById('userInput');
+
+const submitButton=document.querySelector('button');
+
+function displayOutput(message){
+    const container= document.getElementById('outputContainer');
+    const newParagraph= document.createElement('p');
+    newParagraph.innerText='Say Hi Friend!';
+    container.appendChild(newParagraph);
+}
+
+
+submitButton.addEventListener('click', function  (){console.log('user has clicked')});
+const userInput = userInputField.value;
+if (userInput.trim()=== '10') {
+    displayOutput('you entered number 10!');
+
+}
+
+if (parseInt(userInput) ===10){
+    displayOutput('you entered the number 10');
+}else if (userInput.toLowerCase()==='hello'){
+    displayOutput('hello to you, too');
+}else {
+    displayOutput('You entered: ', userInput, ',')
+}
 
 
 
